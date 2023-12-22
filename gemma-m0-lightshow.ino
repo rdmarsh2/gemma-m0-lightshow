78a6f350734f014b351513ec77c2cc8898eb5c79
Robert Marsh <rdmarsh2@gmail.com>
2023-12-22 01:20:08 -0500

    autoformat

--- gemma-m0-lightshow.ino
+++ gemma-m0-lightshow.ino
@@ -16,31 +16,36 @@ uint8_t hues[NUM_PIXELS];
 
 void setup()
 {
-    for(int i = 0; i < NUM_PIXELS; i++) {
-        hues[i] = i*256/NUM_PIXELS;
+    for (int i = 0; i < NUM_PIXELS; i++)
+    {
+        hues[i] = i * 256 / NUM_PIXELS;
     }
-	pixels.begin();
+    pixels.begin();
 }
 
 void loop()
 {
     uint32_t avg = 0;
-    for (int i = 0; i < DATA_SIZE; i++) {
+    for (int i = 0; i < DATA_SIZE; i++)
+    {
         uint32_t mic_val = analogRead(MIC_PIN);
         inputData[i] = mic_val;
     }
 
-    avg = avg/DATA_SIZE;
-    for(int i=0; i<DATA_SIZE; i++) {
+    avg = avg / DATA_SIZE;
+    for (int i = 0; i < DATA_SIZE; i++)
+    {
         inputData[i] = (inputData[i] - avg) * 64;
     }
 
     ZeroFFT(inputData, DATA_SIZE);
 
-	for (int i = 0; i < NUM_PIXELS; i++) {
+    for (int i = 0; i < NUM_PIXELS; i++)
+    {
         uint32_t bucket_avg = 0;
         // TODO: rewrite to handle arbitrary pixel count and input data size. Should be logarithmic...
-        for(int j = 1 << i; j < 2<<i; j++) { // this drops the lowest bucket... I think that's OK.
+        for (int j = 1 << i; j < 2 << i; j++)
+        { // this drops the lowest bucket... I think that's OK.
             bucket_avg += inputData[i];
         }
         pixels.setPixelColor(i, pixels.ColorHSV(hues[i]++, 255, 100 + bucket_avg / (1024 << i)));
